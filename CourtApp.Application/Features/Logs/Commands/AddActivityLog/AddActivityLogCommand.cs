b2944fa2c5e0df4297c1b4cfa16447e18a7b3be1
YokogawaIndia <risl.yokogawa@gmail.com>
2024-03-19 21:14:12 +0530

    audit log implementation

---
+++ CourtApp.Application/Features/Logs/Commands/AddActivityLog/AddActivityLogCommand.cs
@@ -0,0 +1,39 @@
+﻿using AspNetCoreHero.Results;
+using CourtApp.Application.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CourtApp.Application.Features.Logs.Commands.AddActivityLog
+{
+    public partial class AddActivityLogCommand : IRequest<Result<int>>
+    {
+        public string Action { get; set; }
+        public string userId { get; set; }
+    }
+
+    public class AddActivityLogCommandHandler : IRequestHandler<AddActivityLogCommand, Result<int>>
+    {
+        private readonly ILogRepository _repo;
+
+        private IUnitOfWork _unitOfWork { get; set; }
+
+        public AddActivityLogCommandHandler(ILogRepository repo, IUnitOfWork unitOfWork)
+        {
+            _repo = repo;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result<int>> Handle(AddActivityLogCommand request, CancellationToken cancellationToken)
+        {
+            await _repo.AddLogAsync(request.Action, request.userId);
+            await _unitOfWork.Commit(cancellationToken);
+            return Result<int>.Success(1);
+        }
+    }
+
+}
